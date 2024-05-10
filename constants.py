4cce97bd2b4a072e7ceae0d791d746813a284035
Richard <richard.llewellyn@aol.com>
2024-05-10 12:05:14 +0100

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Constants
+JIRA_URL = '<YOUR JIRA URL>'
+EXCLUDED_ISSUE_TYPES = ['Sub-task']  # Add or remove types based on your requirements
+API_TOKEN = '<your jira api key>'
+USER_EMAIL = '<your email>'
+headers = {
+    "Accept": "application/json"
+}
